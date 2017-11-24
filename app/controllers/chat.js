1dad10e84d3c068ed4f9dd6a008f0005240f5232
Anderson A. Nascimento <anderson.a.nascimento@gmail.com>
2017-11-24 10:38:04 -0400

    Beautify

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,13 +1,13 @@
 module.exports.iniciaChat = function (application, req, res) {
-    let dadosForm = req.body;
+    const dadosForm = req.body;
     // console.log(dadosForm);
     req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
     req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
 
-    let errors = req.validationErrors();
+    const errors = req.validationErrors();
 
-    if(errors) {
-        res.render('index', {validacao: errors});
+    if (errors) {
+        res.render('index', { validacao: errors });
         return;
     }
 
